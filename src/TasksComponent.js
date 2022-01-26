bb0196b4c7874e0489a1fb483028a55d666ea79d
oki12 <oskartyniec10@wp.pl>
2022-01-26 20:02:24 +0100

    feat: added a sorting by priority function to the array of tasks

--- src/TasksComponent.js
+++ src/TasksComponent.js
@@ -4,13 +4,46 @@ import styles from './TaskComponent.module.css';
 
 const TaskComponent = () => {
     
-    const [inputValue, setInputValue] = useState("");
-    const [tasks, setTasks] = useState([]);
+    const [inputContentValue, setInputContentValue] = useState(['']);
+    const [inputPriorityValue, setInputPriorityValue] = useState('');
     
-    const addTask = (task) => {
-        if(task != ""){
+    const [tasks, setTasks] = useState([
+        {
+            content: 'Wyprowadzić psa na spacer', 
+            priority: 12,
+            priorityName: "heigh"
+        },
+        {
+            content: 'Zrobić zakupy', 
+            priority: 12,
+            priorityName: "heigh"
+        }
+    ]);
+    
+    const addTask = (contentValue, priorityValue) => {
+        if(contentValue != "" && priorityValue != "none"){
+            let priorityNameValue;
+            if(priorityValue == 3){
+                priorityNameValue = "small";
+            }
+            else if(priorityValue == 6){
+                priorityNameValue = "normal";
+            }
+            else if(priorityValue == 9){
+                priorityNameValue = "medium";
+            }
+            else if(priorityValue == 12){
+                priorityNameValue = "high";
+            }
+
+            const task = {
+                content: contentValue,
+                priority: parseInt(priorityValue),
+                priorityName: priorityNameValue,
+            }
             setTasks((previousTasks) => [...previousTasks, task]);
-            setInputValue("");
+            setInputContentValue("");
+            setInputPriorityValue("none");
         }
     }
     
@@ -19,24 +52,47 @@ const TaskComponent = () => {
         setTasks(tasks.filter(task => task !== value.task));
     }
 
-    const handleChange = (event) => {
-        setInputValue(event.target.value);
+    const handleChangeContentValue = (event) => {
+        setInputContentValue(event.target.value);
     }
 
+    
+    const handleChangePriorityValue = (event) => {
+        setInputPriorityValue(event.target.value);
+    }
+    
     return (
         <div className={`${styles.TaskComponent}`}>
             <h2> Create your the future with <span className={`${styles.text_blue}`}>myTask.com</span></h2>
-            <button onClick={ () => addTask(inputValue)}> Create task </button>
+            
+            <p>Content
+                <input onChange={handleChangeContentValue} value={inputContentValue}/>
+            </p>
 
-            <input onChange={handleChange} value={inputValue}/>
+            <p>Priority
+                <select onChange={handleChangePriorityValue} value={inputPriorityValue}>
+                    <option value="none">Choose priority...</option>
+                    <option value="12">High priority</option>
+                    <option value="9">Medium priority</option>
+                    <option value="6">Normal priority</option>
+                    <option value="3">Small priority</option>
+                </select>
+            </p>
 
-            {tasks.map((task, index) => (
-                <div className={`${styles.task}`} key={index}>
-                    <p> Task - {index + 1} </p>
-                    <p> {task} </p>
-                    <button onClick={ () => deleteTask({task})}> Make as completed </button>
-                </div>
-            ))}
+            <button onClick={ () => addTask(inputContentValue, inputPriorityValue)}> Create task </button>
+
+            <span className={`${styles.blue_title}`}> My tasks </span>
+
+            {
+                tasks.sort( (a,b) => {return b.priority - a.priority}),
+                tasks.map((task, index) => (
+                    <div className={`${styles.task}`} key={index}>
+                        <p> Task priority: {task.priorityName} </p>
+                        <p> {task.content} </p>
+                        <button onClick={ () => deleteTask({task})}> Make as completed </button>
+                    </div>
+                ))
+            }
         </div>
     );
 }
